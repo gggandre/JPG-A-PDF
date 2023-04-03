2a58f04f2a27c987b527997bd205324f522e6a21
André <84719490+gggandre@users.noreply.github.com>
2023-04-03 12:27:27 -0600

    CÓDIGO MEJORADO

--- conversor.py
+++ conversor.py
@@ -4,27 +4,41 @@ from PIL import Image
 
 def convert_images_to_pdf(folder_path):
     """
-    It takes a folder path as input, finds all the .jpg files in the subfolders of that folder, and then
+    It takes a folder path, looks for all the .jpg files in the
+    subfolders of that folder, and then
     creates a PDF file with all those images
-    
-    :param folder_path: The path to the folder containing the subfolders with the images
-    :return: the path to the PDF file.
+    :param folder_path: The path to the folder containing the images
+    :return: the path of the PDF file that was created.
     """
-    images = [os.path.join(folder_path, subfolder, f) for subfolder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, subfolder)) for f in os.listdir(os.path.join(folder_path, subfolder)) if f.endswith('.jpg')]
+    images = []
+    for subfolder in os.listdir(folder_path):
+        subfolder_path = os.path.join(folder_path, subfolder)
+        if os.path.isdir(subfolder_path):
+            for f in os.listdir(subfolder_path):
+                if f.endswith('.jpg'):
+                    image_path = os.path.join(subfolder_path, f)
+                    try:
+                        with Image.open(image_path) as img:
+                            img.load()  # Cargar la imagen para verificar si
+                            # está completa
+                        images.append(image_path)
+                    except Exception as e:
+                        print(f"Error al cargar la imagen {image_path}: {e}")
 
     if not images:
-        print("No se encontraron imágenes .jpg en las subcarpetas de la carpeta especificada")
-        return
+        print("No se encontraron imágenes .jpg en el directorio")
 
     images.sort()
     pdf_path = os.path.join(folder_path, "images.pdf")
 
     with Image.open(images[0]) as img:
-        img.save(pdf_path, "PDF", resolution=100.0, save_all=True, append_images=[Image.open(image) for image in images[1:]])
+        img.save(pdf_path, "PDF", resolution=100.0, save_all=True,
+                 append_images=[Image.open(image).convert('RGB')
+                                for image in images[1:]])
 
     print("El archivo PDF ha sido creado exitosamente")
 
 
 if __name__ == '__main__':
-    folder_path = "Tomos/13"
+    folder_path = "Tomos/17"
     convert_images_to_pdf(folder_path)
